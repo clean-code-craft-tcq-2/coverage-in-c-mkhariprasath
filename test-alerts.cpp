7b92b56278ed9224b2d79b1c672749e62725b85d
Hariprasath Muthumanickam <39271101+mkhariprasath@users.noreply.github.com>
2022-02-21 10:57:42 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -14,3 +14,23 @@ TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(31, 20, 30) == TOO_HIGH); // high boundary check
   REQUIRE(inferBreach(35, 20, 30) == TOO_HIGH);  // high check
 }
+
+TEST_CASE("Classification of Temperature Breach") {
+  REQUIRE((classifyTemperatureBreach(HI_ACTIVE_COOLING, 0) == NORMAL));
+  REQUIRE((classifyTemperatureBreach(HI_ACTIVE_COOLING,20) == NORMAL)); // low boundary check
+  REQUIRE((classifyTemperatureBreach(HI_ACTIVE_COOLING,45) == NORMAL)); // high boundary check
+  REQUIRE((classifyTemperatureBreach(HI_ACTIVE_COOLING,-1) == TOO_LOW));
+  REQUIRE((classifyTemperatureBreach(HI_ACTIVE_COOLING,46) == TOO_HIGH));
+
+  REQUIRE((classifyTemperatureBreach(PASSIVE_COOLING,20) == NORMAL));
+  REQUIRE((classifyTemperatureBreach(PASSIVE_COOLING, 0) == NORMAL)); // low boundary check
+  REQUIRE((classifyTemperatureBreach(PASSIVE_COOLING,35) == NORMAL)); // high boundary check
+  REQUIRE((classifyTemperatureBreach(PASSIVE_COOLING,-1) == TOO_LOW));
+  REQUIRE((classifyTemperatureBreach(PASSIVE_COOLING,36) == TOO_HIGH));
+  
+  REQUIRE((classifyTemperatureBreach(MED_ACTIVE_COOLING,20) == NORMAL));
+  REQUIRE((classifyTemperatureBreach(MED_ACTIVE_COOLING, 0) == NORMAL)); // low boundary check
+  REQUIRE((classifyTemperatureBreach(MED_ACTIVE_COOLING,40) == NORMAL)); // high boundary check
+  REQUIRE((classifyTemperatureBreach(MED_ACTIVE_COOLING,-1) == TOO_LOW));
+  REQUIRE((classifyTemperatureBreach(MED_ACTIVE_COOLING,41) == TOO_HIGH));
+}
