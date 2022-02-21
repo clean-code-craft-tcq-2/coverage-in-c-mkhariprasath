e68cf0c6aee6e2a0cf8e690671e0911fa710ca95
Hariprasath Muthumanickam <39271101+mkhariprasath@users.noreply.github.com>
2022-02-21 10:17:10 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -4,7 +4,13 @@
 #include "typewise-alert.h"
 
 TEST_CASE("infers the breach according to limits") {
-  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
-  REQUIRE(inferBreach(20, 20, 30) == NORMAL);
-  REQUIRE(inferBreach(31, 20, 30) == TOO_HIGH);
+  REQUIRE(inferBreach(18, 20, 30) == TOO_LOW); // low check
+  REQUIRE(inferBreach(19, 20, 30) == TOO_LOW); // low boundary check
+  REQUIRE(inferBreach(20, 20, 30) == NORMAL);  // normal low boundary check
+  REQUIRE(inferBreach(21, 20, 30) == NORMAL);  // normal check
+  REQUIRE(inferBreach(22, 20, 30) == NORMAL);  // normal check
+  REQUIRE(inferBreach(29, 20, 30) == NORMAL);  // normal check
+  REQUIRE(inferBreach(30, 20, 30) == NORMAL);  // normal high boundary check
+  REQUIRE(inferBreach(31, 20, 30) == TOO_HIGH); // high boundary check
+  REQUIRE(inferBreach(35, 20, 30) == TOO_HIGH);  // high check
 }
